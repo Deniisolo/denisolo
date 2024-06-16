b26d88eb3ec829708850049081bf24a9c10aea73
deniisolo <deniisolo@outlook.es>
2024-06-16 10:05:39 -0500

    feat(style): add key frames to photohome and rightside home

--- src/components/organisms/Carrusel.jsx
+++ src/components/organisms/Carrusel.jsx
@@ -1,12 +1,19 @@
-import { Carousel } from "flowbite-react";
+import Slider from "react-slick";
 export function Carrusel(props) {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-      <Carousel>
+      <Slider {...settings}>
         {props.images.map((image) => {
           return <img src={`${process.env.PUBLIC_URL}${image}`} alt="..." />;
         })}
-      </Carousel>
+      </Slider>
     </div>
   );
 }
