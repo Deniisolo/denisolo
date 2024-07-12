356c538c05037ed5f28fca2304d4c3f78f985fb8
deniisolo <deniisolo@outlook.es>
2024-07-11 20:48:39 -0500

    feat(styles): add style Openbutton

--- src/components/atoms/Openmodal.jsx
+++ src/components/atoms/Openmodal.jsx
@@ -1,3 +1,8 @@
-export function Openmoda() {
-  return <button> More</button>;
+import "./Openmodal.css";
+export function Openmodal() {
+  <style>
+    @import
+    url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
+  </style>;
+  return <button className="openModalbutton"> More</button>;
 }
