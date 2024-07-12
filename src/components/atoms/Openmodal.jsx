2938bcf48d9cea15dde8d4e99609182dadb858f1
deniisolo <deniisolo@outlook.es>
2024-07-11 20:08:58 -0500

    feat(chore): add openmodal component

---
+++ src/components/atoms/Openmodal.jsx
@@ -0,0 +1,3 @@
+export function Openmoda() {
+  return <button> More</button>;
+}
--- src/views/Cardportfolio.jsx
+++ src/views/Cardportfolio.jsx
@@ -1,12 +1,21 @@
-import { Paragraphinfo } from "../components/atoms/Paragraphinfo";
-import { Littletittle } from "../components/atoms/Littletittle";
-import { Carrusel } from "../components/organisms/Carrusel";
-export function Cardportfolio(props) {
+import { Contactinformation } from "../components/atoms/Contactinformation";
+import { Label } from "../components/atoms/Label";
+import { DemoButon } from "../components/atoms/DemoButon";
+import { TitlePortfolio } from "../components/atoms/TitlePortfolio";
+import { RepositoryButon } from "../components/atoms/RepositoryButon";
+import { PhotoPortfolio } from "../components/atoms/PhotoPortfolio";
+export function Cardportfolio() {
   return (
-    <section>
-      <Carrusel images={props.images}></Carrusel>
-      <Littletittle Littletittle={props.Littletittle}></Littletittle>
-      <Paragraphinfo Paragraphinfop={props.Paragraphinfop}></Paragraphinfo>
-    </section>
+    <div>
+      <img src="" alt="" />
+      <TitlePortfolio></TitlePortfolio>
+      <Label></Label>
+      <Contactinformation></Contactinformation>
+      <DemoButon></DemoButon>
+      <RepositoryButon></RepositoryButon>
+
+      {/* <Littletittle Littletittle={props.Littletittle}></Littletittle>
+      <Paragraphinfo Paragraphinfop={props.Paragraphinfop}></Paragraphinfo> */}
+    </div>
   );
 }
