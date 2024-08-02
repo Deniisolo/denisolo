ae358517f0f2ba6c101134d067ec5fd3c8668e7c
Elver David <elverdavid0839@gmail.com>
2024-08-01 21:46:13 -0500

    feat(ui): add cursor pointer style to clickable elements

--- src/views/Portfolio.jsx
+++ src/views/Portfolio.jsx
@@ -9,7 +9,6 @@ import { FaFigma } from "react-icons/fa6";
 import { IoLogoCss3 } from "react-icons/io5";
 import { IoLogoHtml5 } from "react-icons/io";
 import { Techlogo } from "../components/atoms/Techlogo";
-import { Allsocialmedia } from "../components/molecules/Allsocialmedia";
 
 import {
   project1,
