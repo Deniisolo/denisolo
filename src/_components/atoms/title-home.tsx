94a155d3f5c5ea3e6c0d99779003557c77f2bb82
Andres-XL <amovillaw01@gmail.com>
2024-08-08 18:13:39 -0500

    🚚 CHORE: remove home-description and title-home components
    these components are too small, and are only used once

--- src/_components/atoms/title-home.tsx
+++
@@ -1,5 +0,0 @@
-import "./Titlehome.css";
-
-export function Titlehome() {
-  return <h1 className="titleHome"> Hello! I'm Denis, </h1>;
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,36 @@
-import { Photohome } from "../components/atoms/Photohome";
-import { Playbutton } from "../components/atoms/Playbutton";
-import { Allsocialmedia } from "../components/molecules/Allsocialmedia";
-import { Homedescription } from "../components/molecules/Homedescription";
-import "./home.css";
+import Image from "next/image";
+import { Photohome } from "~/_components/atoms/photo-home";
+import { Playbutton } from "~/_components/atoms/play-button";
+import { AllSocialMedia } from "~/_components/molecules/all-social-media";
+import { Homedescription } from "~/_components/molecules/home-description";
+import "~/styles/home.css";
+// import "./photo-home.css";
+import homePicture from "./img/phoneDenis.png";
 
-export function Home() {
+export default function Home() {
   return (
-    <div className="homestyle">
-      <div className="leftSide">
-        <Photohome />
+    <div className="min-h-100 flex">
+      <div className="h-full items-center justify-center p-5">
+        <Image
+          className="responsive-photo"
+          src={"/img/phoneDenis.png"}
+          // src={homePicture}
+          fill={true}
+          alt="photodeDenisolo"
+          // width={100}
+          // height={100}
+        />
       </div>
-      <div className="rightSide">
-        <Homedescription />
+      <div className="h-full items-center justify-center p-5">
+        <h1 className="titleHome"> Hello! I&apos;m Denis, </h1>
+        <p>
+          also known as Deniisolo on social media. I&apos;m a front-end
+          developer specialised in creating amazing web experiences.
+        </p>
         <div className="playButtonWrapper">
           <Playbutton />
         </div>
-        <Allsocialmedia />
+        <AllSocialMedia />
       </div>
     </div>
   );
